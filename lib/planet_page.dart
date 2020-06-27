8a3f6c090fc3ebc6b2bd176f925504ed5609fb94
Josue <josue19978@hotmail.com>
2020-06-27 14:19:55 -0500

    Eliminados archivos innecesarios parte 2

--- lib/planet_page.dart
+++ lib/planet_page.dart
@@ -17,7 +17,6 @@ class PlanetPage extends StatefulWidget {
 }
 
 class PlanetPageState extends State<PlanetPage> with TickerProviderStateMixin {
-  Offset _verticalDragStart;
   AnimationController _swipeAnimController;
   AnimationController _slideInAnimController;
   AnimationController _onNavigationAnimController;
@@ -54,34 +53,6 @@ class PlanetPageState extends State<PlanetPage> with TickerProviderStateMixin {
       begin: RelativeRect.fromLTRB(-50.0, screen.height * 0.7, -50.0, 0.0),
       end: RelativeRect.fromLTRB(-50.0, screen.height, -50.0, -screen.height),
     ).animate(_swipeAnimController);
-  }
-
-  Animation<RelativeRect> _moonsRect(Size screen) {
-    return RelativeRectTween(
-      begin: RelativeRect.fromLTRB(
-          0.0, screen.height * 0.45, 0.0, screen.height * 0.425),
-      end: RelativeRect.fromLTRB(-50.0, screen.height * 0.7, -50.0, 0.0),
-    ).animate(_swipeAnimController);
-  }
-
-  void _onVerticalDragStart(DragStartDetails details) {
-    _verticalDragStart = details.globalPosition;
-  }
-
-  void _onVerticalDragUpdate(DragUpdateDetails details) {
-    if (_verticalDragStart.dy - details.globalPosition.dy > 50.0) {
-      _swipeAnimController.reverse();
-      _slideInAnimController.forward();
-    }
-
-    if (_verticalDragStart.dy - details.globalPosition.dy < 0.0) {
-      _swipeAnimController.forward();
-      _slideInAnimController.reverse();
-    }
-  }
-
-  void _onVerticalDragEnd(DragEndDetails details) {
-    _verticalDragStart = null;
   }
 
   Column _descriptionColumn(CelestialBody celestialBody) {
@@ -129,78 +100,53 @@ class PlanetPageState extends State<PlanetPage> with TickerProviderStateMixin {
     );
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
 
-    return GestureDetector(
-      onVerticalDragStart: _onVerticalDragStart,
-      onVerticalDragUpdate: _onVerticalDragUpdate,
-      onVerticalDragEnd: _onVerticalDragEnd,
-      child: Material(
-        color: Colors.black,
-        child: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            PositionedTransition(
-              rect: _planetRect(screenSize),
-              child: Hero(
-                tag: widget.currentPlanet.name,
-                child: CelestialBodyWidget(widget.currentPlanet.vidAssetPath),
-              ),
+    return Material(
+      color: Colors.black,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          PositionedTransition(
+            rect: _planetRect(screenSize),
+            child: Hero(
+              tag: widget.currentPlanet.name,
+              child: CelestialBodyWidget(widget.currentPlanet.vidAssetPath),
             ),
-            Positioned(
-              right: -160 + (10.0 * (_swipeAnimController.value)),
-              top: -100,
-              child: IgnorePointer(
-                ignoring: true,
-                child: SlideTransition(
-                  position:
-                      Tween<Offset>(begin: Offset.zero, end: Offset(0.0, 1.0))
-                          .animate(_onNavigationAnimController),
-                  child: ScaleTransition(
-                    scale: Tween<double>(begin: 1.0, end: 1.05)
-                        .animate(_swipeAnimController),
-                    child: Image.asset(
-                      'assets/flare.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 0.0,
-              right: screenSize.width * 0.15,
-              left: screenSize.width * 0.15,
-              child: SlideTransition(
-                position: Tween<Offset>(
-                  begin: Offset(0.0, 1.0),
-                  end: Offset(0.0, 0.0),
-                ).animate(_slideInAnimController),
-                child: FadeTransition(
-                  opacity: _slideInAnimController,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Hero(
-                        tag: '${widget.currentPlanet.name}heading',
-                        child: Text(
-                          widget.currentPlanet.name.toUpperCase(),
-                          style: Theme.of(context).textTheme.subhead.copyWith(
-                              color: Colors.white, letterSpacing: 10.0),
-                        ),
+          ),
+          Positioned(
+            bottom: 0.0,
+            right: screenSize.width * 0.15,
+            left: screenSize.width * 0.15,
+            child: SlideTransition(
+              position: Tween<Offset>(
+                begin: Offset(0.0, 1.0),
+                end: Offset(0.0, 0.0),
+              ).animate(_slideInAnimController),
+              child: FadeTransition(
+                opacity: _slideInAnimController,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Hero(
+                      tag: '${widget.currentPlanet.name}heading',
+                      child: Text(
+                        widget.currentPlanet.name.toUpperCase(),
+                        style: Theme.of(context)
+                            .textTheme
+                            .subhead
+                            .copyWith(color: Colors.white, letterSpacing: 10.0),
                       ),
-                      _descriptionColumn(widget.currentPlanet),
-                    ],
-                  ),
+                    ),
+                    _descriptionColumn(widget.currentPlanet),
+                  ],
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
