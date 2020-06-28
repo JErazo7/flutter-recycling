95c7ad2a0b3ffa7ec5ae30462e7770dccb3604dd
Felipev94 <65479892+Felipev94@users.noreply.github.com>
2020-06-27 23:32:08 -0500

    Update Screen_camera.dart

--- lib/Screen_camera.dart
+++ lib/Screen_camera.dart
@@ -16,114 +16,116 @@ class ScreenCameraState extends State<ScreenCamera> with TickerProviderStateMixi
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: AnimatedBackground(
+      child: AnimatedBackground(
         behaviour: RandomParticleBehaviour(
-        options: ParticleOptions(
-        particleCount: 300,
-        spawnMaxSpeed: 5.0,
-        spawnMinSpeed: 1.0,
-        spawnMaxRadius: 1.5,
-        spawnMinRadius: 1.0,
-        baseColor: Colors.white)),
-    vsync: this,
-      child: Scaffold(
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height * 0.75,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(MediaQuery.of(context).size.width /9) ,bottomRight:Radius.circular(MediaQuery.of(context).size.width /9)  ),
-                  color: Colors.blue,
+            options: ParticleOptions(
+                particleCount: 300,
+                spawnMaxSpeed: 5.0,
+                spawnMinSpeed: 1.0,
+                spawnMaxRadius: 1.5,
+                spawnMinRadius: 1.0,
+                baseColor: Colors.white)),
+        vsync: this,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Container(
+            color: Colors.transparent,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.75,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(MediaQuery.of(context).size.width /9) ,bottomRight:Radius.circular(MediaQuery.of(context).size.width /9)  ),
+                    color: Colors.blue,
+                  ),
+                  child: Camera(),
                 ),
-                child: Camera(),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  InkWell(
-                    onTap: (){
-                      //TODO: Add Route navigator
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.0125),
-                      height: MediaQuery.of(context).size.height * 0.125,
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      decoration: BoxDecoration(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: (){
+                        //TODO: Add Route navigator
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.0125),
+                        height: MediaQuery.of(context).size.height * 0.125,
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(topRight: Radius.circular(MediaQuery.of(context).size.height * 0.125/3) ,bottomRight:Radius.circular(MediaQuery.of(context).size.height * 0.125/3)  ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Glass',
+                              style: TextStyle(
+                                fontSize: MediaQuery.of(context).size.height * 0.035,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.0125,
+                            ),
+                            Text(
+                              'Place in green bin',
+                              style: TextStyle(
+                                fontSize: MediaQuery.of(context).size.height * 0.025,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.camera,
                         color: Colors.white,
-                        borderRadius: BorderRadius.only(topRight: Radius.circular(MediaQuery.of(context).size.height * 0.125/3) ,bottomRight:Radius.circular(MediaQuery.of(context).size.height * 0.125/3)  ),
+                        size: MediaQuery.of(context).size.height * 0.125/1.65,
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            'Glass',
-                            style: TextStyle(
-                              fontSize: MediaQuery.of(context).size.height * 0.035,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.0125,
-                          ),
-                          Text(
-                            'Place in green bin',
-                            style: TextStyle(
-                              fontSize: MediaQuery.of(context).size.height * 0.025,
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-                        ],
+                      onPressed: (){
+                        //TODO: Add a new Photo
+                      },
+                    ),
+                    IconButton(
+                      icon: _multiPhotos ? Icon(
+                        Icons.check_circle_outline,
+                        color: Colors.white,
+                        size: MediaQuery.of(context).size.height * 0.125/1.65,
+                      ) :
+                      Icon(
+                        Icons.check_circle,
+                        color: Colors.green,
+                        size: MediaQuery.of(context).size.height * 0.125/1.65,
                       ),
-                    ),
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.camera,
-                      color: Colors.white,
-                      size: MediaQuery.of(context).size.height * 0.125/1.65,
-                    ),
-                    onPressed: (){
-                      //TODO: Add a new Photo
-                    },
-                  ),
-                  IconButton(
-                    icon: _multiPhotos ? Icon(
-                      Icons.check_circle_outline,
-                      color: Colors.white,
-                      size: MediaQuery.of(context).size.height * 0.125/1.65,
-                    ) :
-                    Icon(
-                      Icons.check_circle,
-                      color: Colors.green,
-                      size: MediaQuery.of(context).size.height * 0.125/1.65,
-                    ),
-                    onPressed: (){
-                      //TODO: Take one or more fotos is the _multiPhotos is true
-                      setState(() {
-                        if(_multiPhotos == false){
-                          _multiPhotos = true;
-                        } else {
-                          _multiPhotos = false;
-                        }
-                      });
+                      onPressed: (){
+                        //TODO: Take one or more fotos is the _multiPhotos is true
+                        setState(() {
+                          if(_multiPhotos == false){
+                            _multiPhotos = true;
+                          } else {
+                            _multiPhotos = false;
+                          }
+                        });
 
-                    },
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.1,
-                  ),
-                ],
-              ),
-              SizedBox(),
-            ],
+                      },
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.1,
+                    ),
+                  ],
+                ),
+                SizedBox(),
+              ],
+            ),
           ),
         ),
       ),
-        ),
     );
   }
 }
